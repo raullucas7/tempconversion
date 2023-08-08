a319a732ad923de786234a83e62b1a08b4660bac
raullucas7 <139395594+raullucas7@users.noreply.github.com>
2023-08-08 15:42:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 
 int main() {
-    cout << "Enter temperature (F): " << endl;
-    double temp_f;
-    cin >> temp_f;
-    cout << "Celsius (approx.): " << (temp_f - 32) * 5 / 9;
+    string input;
+    cout << "Convert from Celsius or Fahrenheit? ";
+    cin >> input;
+
+    if (input == "Celsius") {
+        cout << "Enter temperature (C): " << endl;
+        double temp_c;
+        cin >> temp_c;
+        double temp_f = (temp_c * 9 / 5) + 32;
+        cout << "Fahrenheit (approx.): " << temp_f;
+
+    } else if (input == "Fahrenheit"){
+        cout << "Enter temperature (F): " << endl;
+        double temp_f;
+        cin >> temp_f;
+        double temp_c = (temp_f - 32) * 5 / 9;
+        cout << "Celsius (approx.): " << temp_c;
+    }
+    return 0;
 }
